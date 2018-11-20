a94e5ce092f351d74cd6acdd482e0ba51bf472d4
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2018-11-20 16:50:43 +0330

    implement a efficient modpow solidity function

---
+++ misc/NumberTheory.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.24;
+
+contract NumberTheory {
+    function modPow(uint base, uint exponent, uint modulus) public pure returns (uint) {
+        if(modulus == 1) return 0;
+        // Assert :: (modulus - 1) * (modulus - 1) does not overflow base
+        uint result = 1;
+        base = base % modulus;
+        while(exponent > 0) {
+            if (exponent % 2 == 1)
+               result = (result * base) % modulus;
+            exponent = exponent / 2;
+            base = (base * base) % modulus;
+        }
+        return result;
+    }
+}
